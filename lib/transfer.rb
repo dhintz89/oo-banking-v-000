7541197e22549b63de27bfe0f9834bae7b6ba947
Dan Hintz <dhintz@live.com>
2018-09-07 01:21:01 +0000

    automatically backed up by learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -17,6 +17,9 @@ class Transfer
       sender.balance -= @amount
       receiver.balance += @amount
       @status = "complete"
+    else
+      puts "Transaction rejected. Please check your account balance."
+      status = ""
     end
   end
   
